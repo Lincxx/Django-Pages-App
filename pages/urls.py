e02a3c1ea21a4a8ba0e40051c8edfbc1e0f57b4b
Lincxx <bossco28@gmail.com>
2019-06-02 08:49:30 -0400

    Added in simple tests for the web pages

---
+++ pages/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import HomePageView
+
+urlpatterns = [
+    path('about/', HomePageView.as_view(), name='about'),
+    path('', HomePageView.as_view(), name='home'),
+]
