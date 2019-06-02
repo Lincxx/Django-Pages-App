ddd2a508d831c1047f3f4da5982294adb4c22f70
Lincxx <bossco28@gmail.com>
2019-06-02 10:01:21 -0400

    Fixed conflict with the urls were using the same

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import HomePageView
 
 urlpatterns = [
-    path('about/', HomePageView.as_view(), name='about'),
+    path('about/', AboutPageView.as_view(), name='about'),
     path('', HomePageView.as_view(), name='home'),
 ]
